ad0fa014220d033e8287f84c2b97c59ef30288a3
Elorfin <penin.axel@gmail.com>
2015-01-14 09:06:34 +0100

    adapt filter for new version of bazingajstranslation

--- Resources/public/js/angular-translation-filter.js
+++ Resources/public/js/angular-translation-filter.js
@@ -3,42 +3,43 @@
 angular.module('ui.translation', [])
     .filter('trans', function () {
         return function (text, placeholder, domain) {
+            var translated = null;
+
             if (typeof text !== 'string') {
                 console.error('Translation filter only works on strings.');
-
-                return text;
+                translated = text;
             }
 
             if (typeof Translator !== 'object') {
                 console.error('Translator object not found.');
-
-                return text;
+                translated = text;
             }
 
             if (typeof placeholder !== 'object') {
                 placeholder = {};
             }
 
-            var search = text;
             if (typeof domain === 'string' && domain.length !== 0) {
-                search = domain + ':' + text;
+                translated = Translator.trans(text, placeholder, domain);
+            } else {
+                translated = Translator.trans(text, placeholder);
             }
 
-            return Translator.get(search, placeholder);
+            return translated;
         };
     })
     .filter('transChoice', function () {
         return function (text, count, placeholder, domain) {
+            var translated = null;
+
             if (typeof text !== 'string') {
                 console.error('Translation filter only works on strings.');
-
-                return text;
+                translated = text;
             }
 
             if (typeof Translator !== 'object') {
                 console.error('Translator object not found.');
-
-                return text;
+                translated = text;
             }
 
             if (typeof count !== 'number' && parseInt(count) != count) {
@@ -49,11 +50,12 @@ angular.module('ui.translation', [])
                 placeholder = {};
             }
 
-            var search = text;
             if (typeof domain === 'string' && domain.length !== 0) {
-                search = domain + ':' + text;
+                translated = Translator.transChoice(text, placeholder, count, domain);
+            } else {
+                translated = Translator.transChoice(text, placeholder, count);
             }
 
-            return Translator.get(search, placeholder, count);
+            return translated;
         };
     });
